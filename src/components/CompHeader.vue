c3d73922c62b277d49ee8299caecf2a8855c8fdb
Alexblai <axelblaise012@gmail.com>
2024-12-11 12:44:20 +0100

    add chat

--- src/components/CompHeader.vue
+++ src/components/CompHeader.vue
@@ -1,4 +1,12 @@
 <template>
+   <div>
+      <a 
+        href="https://www.stack-ai.com/embed/21e980bf-e1f6-415a-82aa-f47a07c5ee2d/555f4d88-78cd-49df-a0c8-ec0cd78975a9/67595eab2eab1bd61c32ec33"
+        class="chatbot-button"
+      >
+        <i class="fa fa-comments"></i>
+      </a>
+    </div>
   <div class="container fixed-top py-md-2" style="background-color: #1c3b72;">
     <router-link :to="{ name: 'details-event' }" class="navbar-brand" >
       <div class="res-main-logo" style="display: flex; align-items: center">
@@ -51,6 +59,26 @@ export default {
 </script>
 
 <style>
+.chatbot-button {
+  position: fixed;
+  bottom: 15%;
+  right: 20px;
+  padding: 10px 20px;
+  background-color: #007bff; /* Couleur de fond */
+  color: #fff; /* Couleur du texte */
+  border: none;
+  border-radius: 5px;
+  text-align: center;
+  cursor: pointer;
+  font-size: 16px;
+  text-decoration: none;
+  z-index: 1000; /* Cette valeur garantit que le bouton sera au-dessus de tous les autres éléments */
+}
+
+.chatbot-button:hover {
+  background-color: #0056b3; /* Couleur de fond sur hover */
+}
+
 /* Position the footer navigation at the bottom */
 .menu-navigation {
   position: fixed;
